9c9f260ff72f377bad531799546faf2ac660f28a
keyvan <keyvan.ygh@gmail.com>
2023-03-31 16:18:22 -0400

    move RounderCorner to seperate file

--- RatioBarChart/RatioBarChart.swift
+++ RatioBarChart/RatioBarChart.swift
@@ -117,7 +117,6 @@ struct RatioBarChart: View {
             }
         }
     }
-
 }
 
 struct RatioBarChart_Previews: PreviewProvider {
@@ -132,12 +131,3 @@ extension View {
     }
 }
 
-struct RoundedCorner: Shape {
-    var radius: CGFloat = .infinity
-    var corners: UIRectCorner = .allCorners
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
---
+++ RatioBarChart/RoundedCorner.swift
@@ -0,0 +1,22 @@
+//
+//  RoundedCorner.swift
+//  RatioBarChart
+//
+//  Created by Keyvan on 3/31/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect, byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
