c1015eb40304548379a68463b31b0269898fe009
keyvan <keyvan.ygh@gmail.com>
2023-03-31 10:56:27 -0400

    constructor added

--- RatioBarChart/RadioBarChartViewModel.swift
+++ RatioBarChart/RadioBarChartViewModel.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 class RadioBarChartViewModel: ObservableObject {
+    let wins:Int
+    let losses:Int
+    
+    init(wins: Int, losses: Int) {
+        self.wins = wins
+        self.losses = losses
+    }
     
 }
